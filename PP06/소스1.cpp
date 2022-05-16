96355a5c0dc92ac55d2759f6bc9d0af80cd9dfaa
swk5276 <swk5276@gmail.com>
2022-05-17 00:37:34 +0900

    201934011 김성웅

--- PP06/소스1.cpp
+++ PP06/소스1.cpp
@@ -1,27 +1,29 @@
-#include <vector> 
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
 template<typename T>
-int sum(T a, T b)
+T sum(T a, T b)
 {
 	return a + b;
 }
+
+
 int main()
 {
 	vector<int> a;
 
-	cout << sum<double,int> << endl;
+	cout << sum<double>(3.9, 4.9) << endl;
+
+	cout << a.size() <<endl;
 
 	a.push_back(1);
 	a.push_back(2);
-	cout << a.size() << endl;
-
 	a.push_back(4);
-	a.push_back(8);
-	a.push_back(16);
+
 	cout << a.size() << endl;
+	cout << "б╞иг :"<< a[0] << endl;
 
 	return 0;
 }
